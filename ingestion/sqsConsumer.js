a6aa8650e1a8d3ec620dcf0378281d5f81561ec5
Darshil Shah <34602679+i-am-darshil@users.noreply.github.com>
2023-04-25 23:29:12 +0530

    Add support to ingest into CLP (#6)
    * Add support to ingest into CLP
    
    * Minor change in path
    
    ---------
    
    Co-authored-by: Darshil Shah <darshilx@amazon.com>

--- ingestion/sqsConsumer.js
+++ ingestion/sqsConsumer.js
@@ -1,17 +1,36 @@
 import { Consumer } from 'sqs-consumer';
 import { SQSClient } from '@aws-sdk/client-sqs';
+import ingest from "./ingest.js"
 import CREDS from "../configs/creds.js"
+import configs from "../configs/config.js"
 
 
 const queueUrl = 'https://sqs.us-west-2.amazonaws.com/648508847588/logging-solution-hackathon';
 console.log("Creating a sqs consumer on queueUrl : ", queueUrl);
 
+function messageHandler(message) {
+  return new Promise((resolve, reject) => {
+    console.log("Recieved a message : ", message)
+
+    let logFile = `${configs.RAW_LOGS_FOLDER}/abc/2023/04/21/06/13`
+    // download the file from S3 to logFile ({serviceName}/${year}/${month}/${day}/${hour}/${minute})
+
+    ingest(logFile)
+      .then(function(stdout) {
+        resolve()
+      })
+      .catch(function (error){
+        console.warn(`Failure in handling sqs message with error : ${error}`)
+        resolve()
+      })
+  })
+ 
+
+}
+
 const sqsConsumer = Consumer.create({
   queueUrl: queueUrl,
-  handleMessage: async (message) => {
-    console.log("Recieved a message : ", message)
-    // ...
-  },
+  handleMessage: messageHandler,
   sqs: new SQSClient({
     region: 'us-west-2',
     credentials: {
