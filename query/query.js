ec69b6e23fb2ea7415e8e23c9ba59154f7591c9a
Darshil Shah <darshilx@amazon.com>
2023-04-26 19:28:21 +0530

    Remove time window from query search

--- query/query.js
+++ query/query.js
@@ -7,41 +7,41 @@ import {unlink} from "fs/promises";
 function query(req,res) {
   const serviceName = req.query.serviceName;
   const searchQuery = req.query.searchQuery;
-  const startWindow = req.query.startWindow; //Format will be : yy/mm/dd/hh/mm
-  const endWindow = req.query.endWindow; //Format will be : yy/mm/dd/hh/mm
+  // const startWindow = req.query.startWindow; //Format will be : yy/mm/dd/hh/mm
+  // const endWindow = req.query.endWindow; //Format will be : yy/mm/dd/hh/mm
 
   let command = `cd ${configs.CLP_ROOT} && sudo ./sbin/search '${searchQuery}'`;
   // Below line is for mocking and testing
   // let command = `echo`;
 
-  let startWindowArr = startWindow.split("/")
-  let endWindowArr = endWindow.split("/")
+  // let startWindowArr = startWindow.split("/")
+  // let endWindowArr = endWindow.split("/")
   
 
-  let startDate = new Date(startWindowArr[0], startWindowArr[1], startWindowArr[2], startWindowArr[3], startWindowArr[4], 0, 0)
-  let endDate = new Date(endWindowArr[0], endWindowArr[1], endWindowArr[2], endWindowArr[3], endWindowArr[4], 0, 0)
+  // let startDate = new Date(startWindowArr[0], startWindowArr[1], startWindowArr[2], startWindowArr[3], startWindowArr[4], 0, 0)
+  // let endDate = new Date(endWindowArr[0], endWindowArr[1], endWindowArr[2], endWindowArr[3], endWindowArr[4], 0, 0)
 
-  let loop = startDate;
-  while(loop <= endDate){
-    let minute = loop.getMinutes()
-    if (minute < 10) minute = `0${minute}`
+  // let loop = startDate;
+  // while(loop <= endDate){
+  //   let minute = loop.getMinutes()
+  //   if (minute < 10) minute = `0${minute}`
 
-    let hour = loop.getHours()
-    if (hour < 10) hour = `0${hour}`
+  //   let hour = loop.getHours()
+  //   if (hour < 10) hour = `0${hour}`
 
-    let day = loop.getDate();
-    if (day < 10) day = `0${day}`
+  //   let day = loop.getDate();
+  //   if (day < 10) day = `0${day}`
   
-    let month = loop.getMonth();
-    if (month < 10) month = `0${month}`
+  //   let month = loop.getMonth();
+  //   if (month < 10) month = `0${month}`
 
-    let year = loop.getFullYear();
-    if (year < 10) year = `0${year}`
+  //   let year = loop.getFullYear();
+  //   if (year < 10) year = `0${year}`
 
-    let logPath = `${configs.RAW_LOGS_FOLDER}/${serviceName}/${year}/${month}/${day}/${hour}/${minute}`
-    command += ` --file-path ${logPath}`
-    loop.setTime(loop.getTime() + 1000 * 60);
-  }
+  //   let logPath = `${configs.RAW_LOGS_FOLDER}/${serviceName}/${year}/${month}/${day}/${hour}/${minute}`
+  //   command += ` --file-path ${logPath}`
+  //   loop.setTime(loop.getTime() + 1000 * 60);
+  // }
   
   const resultsFilePath = `${configs.QUERY_RESULTS_FOLDER}/${serviceName}_${uuidv4()}`;
   // Below line is for mocking and testing
